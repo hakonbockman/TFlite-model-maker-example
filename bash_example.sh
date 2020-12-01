6f138588f74e54dfd82effa2d4ad7cac9080b564
Håkon Sørensen Bøckman <hakonbockman@gmail.com>
2020-12-01 11:42:45 +0100

    updated bash script

--- bash_example.sh
+++ bash_example.sh
@@ -9,16 +9,16 @@
 #SBATCH --output=tflite_test_run.out
 #SBATCH --mail-user=<haakosbo@ntnu.no>
 #SBATCH --mail-type=ALL
-# WORKDIR=${TFlite-model-maker example}
-# cd ${WORKDIR}
-# echo "we are running from this directory: $SLURM_SUBMIT_DIR"
-# echo " the name of the job is: $SLURM_JOB_NAME"
-# echo "Th job ID is $SLURM_JOB_ID"
-# echo "The job was run on these nodes: $SLURM_JOB_NODELIST"
-# echo "Number of nodes: $SLURM_JOB_NUM_NODES"
-# echo "We are using $SLURM_CPUS_ON_NODE cores"
-# echo "We are using $SLURM_CPUS_ON_NODE cores per node"
-# echo "Total of $SLURM_NTASKS cores"
-# module purge
-# module load module_set_1
-# mpirun python main.py
+WORKDIR=${TFlite-model-maker example}
+cd ${WORKDIR}
+echo "we are running from this directory: $SLURM_SUBMIT_DIR"
+echo " the name of the job is: $SLURM_JOB_NAME"
+echo "Th job ID is $SLURM_JOB_ID"
+echo "The job was run on these nodes: $SLURM_JOB_NODELIST"
+echo "Number of nodes: $SLURM_JOB_NUM_NODES"
+echo "We are using $SLURM_CPUS_ON_NODE cores"
+echo "We are using $SLURM_CPUS_ON_NODE cores per node"
+echo "Total of $SLURM_NTASKS cores"
+command module purge
+command module load module_set_1
+command mpirun python main.py
